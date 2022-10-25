f2cd20006d6dec9354f21f6ab5baefe646c9ff72
Brian Pugh <bnp117@gmail.com>
2022-10-24 19:11:51 -0700

    fix mock targets

--- tests/cli/test_exec.py
+++ tests/cli/test_exec.py
@@ -1,5 +1,5 @@
 def test_exec_basic(mocker, mock_device, cli_runner):
-    mock_device.patch("belay.cli.main.Device")
+    mock_device.patch("belay.cli.exec.Device")
     result = cli_runner("exec", "print('hello world')")
     assert result.exit_code == 0
     mock_device.inst.assert_called_once_with("print('hello world')")
--- tests/cli/test_run.py
+++ tests/cli/test_run.py
@@ -1,5 +1,5 @@
 def test_run_basic(mocker, mock_device, cli_runner, tmp_path):
-    mock_device.patch("belay.cli.main.Device")
+    mock_device.patch("belay.cli.run.Device")
     py_file = tmp_path / "foo.py"
     py_file.write_text("print('hello')\nprint('world')")
     result = cli_runner("run", str(py_file))
