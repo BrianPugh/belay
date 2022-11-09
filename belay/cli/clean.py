be7986408f2b11fc76a0c9421aa5f64548b4eeb3
Brian Pugh <bnp117@gmail.com>
2022-11-09 13:42:14 -0800

    Add clean command

---
+++ belay/cli/clean.py
@@ -0,0 +1,17 @@
+from rich.console import Console
+
+from belay.packagemanager import clean_local
+
+from .common import load_toml
+
+
+def clean():
+    """Remove any downloaded dependencies if they are no longer specified in pyproject."""
+    toml = load_toml()
+
+    try:
+        dependencies = toml["dependencies"]
+    except KeyError:
+        return
+
+    clean_local(dependencies.keys())
