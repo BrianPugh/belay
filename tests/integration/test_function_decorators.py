c8b47036a9dd7fdc2cecea30a8045472af937c26
Brian Pugh <bnp117@gmail.com>
2022-12-06 12:32:50 -0800

    Add task decorator integration tests

--- tests/integration/test_function_decorators.py
+++ tests/integration/test_function_decorators.py
@@ -3,7 +3,26 @@ import pytest
 import belay
 
 
-def test_generators_basic(emulated_device):
+def test_setup_basic(emulated_device):
+    @emulated_device.setup
+    def setup(config):
+        foo = config["bar"]  # noqa: F841
+
+    setup({"bar": 25})
+
+    assert {"bar": 25} == emulated_device("config")
+    assert 25 == emulated_device("foo")
+
+
+def test_task_basic(emulated_device):
+    @emulated_device.task
+    def foo(val):
+        return 2 * val
+
+    assert 10 == foo(5)
+
+
+def test_task_generators_basic(emulated_device):
     @emulated_device.task
     def my_gen(val):
         i = 0
@@ -16,7 +35,7 @@ def test_generators_basic(emulated_device):
     assert [0, 1, 2] == list(my_gen(3))
 
 
-def test_generators_communicate(emulated_device):
+def test_task_generators_communicate(emulated_device):
     @emulated_device.task
     def my_gen(x):
         x = yield x
