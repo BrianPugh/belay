56ad270f92fee0b78d43cdca36b81acd6484a077
Brian Pugh <bnp117@gmail.com>
2022-12-16 16:17:46 -0800

    Update subclassing example to show @Device.setup usage

--- examples/08_device_subclassing/main.py
+++ examples/08_device_subclassing/main.py
@@ -10,14 +10,22 @@ args = parser.parse_args()
 
 class MyDevice(Device):
     # NOTE: ``Device`` is capatalized here!
+    @Device.setup(
+        autoinit=True
+    )  # ``autoinit=True`` means this method will automatically be called during object creation.
+    def setup():
+        # Code here is executed on-device in a global context.
+        led_pin = Pin(25, Pin.OUT)
+        sensor_temp = ADC(
+            4
+        )  # ADC4 is attached to an internal temperature sensor on the Pi Pico
+
     @Device.task
     def set_led(state):
-        Pin(25, Pin.OUT).value(state)
+        led_pin.value(state)
 
     @Device.task
     def read_temperature():
-        # ADC4 is attached to an internal temperature sensor
-        sensor_temp = ADC(4)
         reading = sensor_temp.read_u16()
         reading *= 3.3 / 65535  # Convert reading to a voltage.
         temperature = 27 - (reading - 0.706) / 0.001721  # Convert voltage to Celsius
