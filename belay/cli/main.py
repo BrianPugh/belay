ea526f7c750118f0f1f7bd07ef466636fe64c8e7
Brian Pugh <bnp117@gmail.com>
2022-12-23 19:45:30 -0500

    initial 'belay run exec:' functionality

--- belay/cli/main.py
+++ belay/cli/main.py
@@ -1,3 +1,8 @@
+import os
+import subprocess  # nosec
+import sys
+from typing import List
+
 import typer
 
 from belay import Device
@@ -11,6 +16,7 @@ from belay.cli.new import new
 from belay.cli.run import run
 from belay.cli.sync import sync
 from belay.cli.update import update
+from belay.project import load_groups
 
 app = typer.Typer()
 app.command()(clean)
@@ -22,3 +28,27 @@ app.command()(new)
 app.command()(run)
 app.command()(sync)
 app.command()(update)
+
+
+def run_exec(command: List[str]):
+    """Pseudo-virtual-environment."""
+    groups = load_groups()
+    virtual_env = os.environ.copy()
+    # Add all dependency groups to the micropython path.
+    virtual_env["MICROPYPATH"] = os.pathsep.join(str(g.folder) for g in groups)
+    subprocess.check_output(command, env=virtual_env)  # nosec
+
+
+def run_app(*args, **kwargs):
+    """Add CLI hacks that are not Typer-friendly here."""
+    exec_identifier = "exec:"
+    if sys.argv[1] == "run" and sys.argv[2].startswith(exec_identifier):
+        # See docstring in belay.cli.run.run
+        command = sys.argv[2:].copy()
+        command[0] = command[0][len(exec_identifier) :]
+        if not command[0]:
+            command = command[1:]
+        run_exec(command)
+    else:
+        # Common-case; use Typer functionality.
+        app(*args, **kwargs)
--- belay/cli/run.py
+++ belay/cli/run.py
@@ -11,7 +11,14 @@ def run(
     file: Path = Argument(..., help="File to run on-device."),
     password: str = Option("", help=help_password),
 ):
-    """Run file on-device."""
+    """Run file on-device.
+
+    If the first argument, ``port``, begins with "exec:", the remainder
+    of the command will be interpreted as a shell command that will
+    be executed in a pseudo-micropython-virtual-environment. As of
+    right now, this just sets ``MICROPYPATH`` to all of the dependency
+    groups' folders.
+    """
     device = Device(port, password=password)
     content = file.read_text()
     device(content)
