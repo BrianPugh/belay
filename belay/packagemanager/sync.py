5f21a1c171b3b2e58977a6c900a25eb141c7bd05
Brian Pugh <bnp117@gmail.com>
2023-02-04 15:10:47 -0800

    Have packagemanager.sync remove empty directories

--- belay/packagemanager/sync.py
+++ belay/packagemanager/sync.py
@@ -26,8 +26,17 @@ def sync(src_folder: PathType, dst_folder: PathType) -> bool:
     changed = False
     src_folder, dst_folder = Path(src_folder), Path(dst_folder)
 
-    src_files = {x.relative_to(src_folder) for x in src_folder.rglob("*")}
-    dst_files = {x.relative_to(dst_folder) for x in dst_folder.rglob("*")}
+    src_files = {
+        x.relative_to(src_folder) for x in src_folder.rglob("*") if x.is_file()
+    }
+
+    dst_files, dst_subfolders = set(), set()
+    for dst_item in dst_folder.rglob("*"):
+        dst_item_rel = dst_item.relative_to(dst_folder)
+        if dst_item.is_file():
+            dst_files.add(dst_item_rel)
+        else:
+            dst_subfolders.add(dst_item_rel)
 
     common_files = src_files.intersection(dst_files)
     src_only_files = src_files - dst_files
@@ -47,12 +56,21 @@ def sync(src_folder: PathType, dst_folder: PathType) -> bool:
         changed = True
         src = src_folder / f
         dst = dst_folder / f
+        dst.parent.mkdir(parents=True, exist_ok=True)
         shutil.copy(src, dst)
 
     # Remove files that only exist in the destination
     for f in dst_only_files:
         changed = True
         dst = dst_folder / f
-        (dst_folder / f).unlink()
+        dst.unlink()
+
+    # Remove all empty dst directories.
+    for folder in dst_subfolders:
+        try:
+            folder.rmdir()
+        except OSError:
+            # Folder was not empty.
+            pass
 
     return changed
