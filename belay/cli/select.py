d3db7d8a775077c675a8c89710ff906b5727e80a
Brian Pugh <bnp117@gmail.com>
2023-04-15 16:29:42 -0700

    working circuitpython led blink

--- belay/cli/select.py
+++ belay/cli/select.py
@@ -1,24 +1,33 @@
-from threading import Thread
-from time import sleep
+import asyncio
+import contextlib
 
 import questionary
 from questionary import Choice
 
 from belay import Device, DeviceMeta
+from belay.cli.questionary_ext import press_any_key_to_continue
 from belay.helpers import list_devices
 
 
-def blink_loop(device):
+async def blink_loop(device):
     while True:
         device.led(True)
-        sleep(0.5)
+        await asyncio.sleep(0.5)
         device.led(False)
-        sleep(0.5)
+        await asyncio.sleep(0.5)
+
+
+async def blink_until_prompt(device):
+    blink_task = asyncio.create_task(blink_loop(device))
+    await press_any_key_to_continue().ask_async()
+    blink_task.cancel()
+    with contextlib.suppress(asyncio.CancelledError):
+        await blink_task
 
 
 class CircuitPythonBlinker(metaclass=DeviceMeta):
     @Device.setup(implementation="circuitpython")
-    def setup_meow(pin: str, is_neopixel) -> None:
+    def setup(pin: str, is_neopixel) -> None:
         import board
         import digitalio
 
@@ -39,7 +48,7 @@ class CircuitPythonBlinker(metaclass=DeviceMeta):
         else:
             led_io.value = value
 
-    @Device.teardown(implementation="circuitpython")
+    @Device.teardown(implementation="circuitpython", ignore_errors=True)
     def teardown():
         if is_neopixel:
             neopixel_write(led_io, b"\x00\x00\x00")
@@ -49,7 +58,7 @@ class CircuitPythonBlinker(metaclass=DeviceMeta):
 
 class MicroPythonBlinker(metaclass=DeviceMeta):
     @Device.setup
-    def setup_meow(pin, is_neopixel) -> None:
+    def setup(pin, is_neopixel) -> None:
         from machine import Pin
 
         led_io = Pin(pin, Pin.OUT)
@@ -67,7 +76,7 @@ class MicroPythonBlinker(metaclass=DeviceMeta):
         else:
             led_io(value)
 
-    @Device.teardown
+    @Device.teardown(ignore_errors=True)
     def teardown():
         if is_neopixel:
             pixel[0] = (0, 0, 0)
@@ -110,53 +119,42 @@ def select():
             )
         )
 
-    while True:
-        device_index = questionary.select(
-            f"Select USB Device (Use arrow keys):\n{header}",
-            choices=choices,
-            instruction=" ",
-        ).ask()
+    device_index = questionary.select(
+        f"Select USB Device (Use arrow keys):\n{header}",
+        choices=choices,
+        instruction=" ",
+    ).ask()
 
-        spec = specs[device_index]
+    spec = specs[device_index]
 
-        with Blinker(spec) as device:
-            questionary.print(str(device.implementation), style=style)
+    with Blinker(spec) as device:
+        questionary.print(str(device.implementation), style=style)
 
-            def validate_led_pin_number(response):
-                if not response:
-                    return True
-
-                try:
-                    int(response)
-                except ValueError:
-                    return False
+        def validate_led_pin_number(response):
+            if not response:
                 return True
 
-            pin_number = questionary.text(
-                "Blink LED Pin Number [skip]?",
-                validate=validate_led_pin_number,
+            try:
+                int(response)
+            except ValueError:
+                return False
+            return True
+
+        pin_number = questionary.text(
+            "Blink LED Pin Number [skip]?",
+            validate=validate_led_pin_number,
+        ).ask()
+
+        if pin_number:
+            is_neopixel = questionary.confirm(
+                "Is this a NeoPixel?", default=False
             ).ask()
+            device.setup(pin_number, is_neopixel)
 
-            if pin_number:
-                is_neopixel = questionary.confirm(
-                    "Is this a NeoPixel?", default=False
-                ).ask()
-                device.setup_meow(pin_number, is_neopixel)
-
-                # thread = Thread(target=blink_loop, args=(device,))
-                # thread.daemon = True
-                # thread.start()
-                while True:
-                    device.led(True)
-                    sleep(0.5)
-                    device.led(False)
-                    sleep(0.5)
-
-                questionary.text("Press enter to continue")
-
-            break
+            asyncio.run(blink_until_prompt(device))
 
     spec_json = spec.json(exclude_none=True)
+    questionary.print("\n")
     questionary.print("Either set the BELAY_DEVICE environment variable:", style=style)
     questionary.print(f"    export BELAY_DEVICE='{spec_json}'")
     questionary.print(
@@ -167,4 +165,4 @@ def select():
     questionary.print(
         "Or, add the following (or a subset) to your python code:", style=style
     )
-    questionary.print(f"    spec = belay.{device!r}\n    device = belay.Device(spec)\n")
+    questionary.print(f"    spec = belay.{spec!r}\n    device = belay.Device(spec)\n")
