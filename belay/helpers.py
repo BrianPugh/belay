6ae1918835d659668aa38eef6806f3ebafcf9369
Brian Pugh <bnp117@gmail.com>
2025-07-07 09:04:59 -0400

    if possible, use a nativemodule for hashing.

--- belay/helpers.py
+++ belay/helpers.py
@@ -2,8 +2,10 @@ import secrets
 import string
 import sys
 from functools import lru_cache, partial, wraps
+from pathlib import Path
+from typing import Optional
 
-from . import snippets
+from . import nativemodule_fnv1a32, snippets
 
 if sys.version_info < (3, 9, 0):
     import importlib_resources
@@ -26,3 +28,11 @@ def random_python_identifier(n=16):
 def read_snippet(name):
     resource = f"{name}.py"
     return importlib_resources.files(snippets).joinpath(resource).read_text(encoding="utf-8")
+
+
+def get_fnv1a32_native_path(implementation) -> Optional[Path]:
+    if implementation.name != "micropython":
+        return None
+    mpy_fn = f"mpy{implementation.version[0]}.{implementation.version[1]}-{implementation.arch}.mpy"
+    filepath = importlib_resources.files(nativemodule_fnv1a32).joinpath(mpy_fn)
+    return filepath if filepath.exists() else None
